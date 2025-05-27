9d98d88402768b4fd313dcb78e511b64bb7fc291
Michael Pereira <michaelpereira408@gmail.com>
2025-05-27 19:32:24 -0300

    feat: implement clinic creation form and dialog component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,10 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
-
+import { eq } from "drizzle-orm";
 import SignOutButton from "./components/sign-out-button";
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
@@ -11,6 +13,14 @@ const DashboardPage = async () => {
   });
   if (!session?.user) {
     redirect("/authentication");
+  }
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id)
+  });
+
+  if( clinics.length === 0) {
+    redirect("/clinic-form");
   }
 
   return (
