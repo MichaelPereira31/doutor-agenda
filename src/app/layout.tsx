9723154e9ed36a645f5cb45818fb548419e7a8d3
Michael Pereira <michaelpereira408@gmail.com>
2025-06-02 19:47:01 -0300

    feat: add appointment management features
    - Updated package dependencies for various libraries.
    - Implemented AddAppointmentButton component for creating new appointments.
    - Created AddAppointmentForm component for handling appointment details and validation.
    - Added AppointmentsTableActions component for managing appointment actions (delete).
    - Defined appointmentsTableColumns for displaying appointment data in a table format.
    - Developed AppointmentsPage to manage appointments with a data table and add button.
    - Introduced Calendar and Popover components for date selection and UI interactions.
    - Updated database schema to include appointmentPriceInCents in appointments.
    - Added ReactQueryProvider for managing API queries with React Query.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
-import { Toaster } from "@/components/ui/sonner"
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import { Toaster } from "@/components/ui/sonner";
+import { ReactQueryProvider } from "@/providers/react-query";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +31,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main>{children}</main>
-        <Toaster richColors theme="light"/>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Toaster position="bottom-center" richColors theme="light" />
       </body>
     </html>
   );
