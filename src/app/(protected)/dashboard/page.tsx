e27fd9ff70494bdba040e314cddfec6032da111f
Michael Pereira <michaelpereira408@gmail.com>
2025-06-05 16:48:17 -0300

    feat: enhance DashboardPage with stats cards for revenue, appointments, patients, and doctors

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -2,40 +2,110 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
-import { PageActions, PageContainer, PageContent, PageDescription, PageHeader, PageHeaderContent, PageTitle } from "@/components/ui/page-container";
-import AddPatientButton from "../patients/_components/add-patient-button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
 import { DatePicker } from "./_components/date-picker";
+import { db } from "@/db";
+import { appointmentsTable, doctorsTable, patientsTable } from "@/db/schema";
+import { eq, and, gte, lte, sum, count } from "drizzle-orm";
+import StatsCards from "./_components/stats-cards";
+import dayjs from "dayjs";
 
-const DashboardPage = async () => {
+interface DashboardPageProps {
+  searchParams: Promise<{
+    from: string;
+    to: string;
+  }>;
+}
+const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  
+
   if (!session?.user) {
     redirect("/authentication");
   }
 
-  if(!session.user.clinic) {
+  if (!session.user.clinic) {
     redirect("/clinic-form");
   }
 
+  const { from, to } = await searchParams;
+
+    if (!from || !to) {
+    redirect(
+      `/dashboard?from=${dayjs().format("YYYY-MM-DD")}&to=${dayjs().add(1, "month").format("YYYY-MM-DD")}`,
+    );
+  }
+
+  const [[totalRevenue],[totalAppointments],[totalPatients],[totalDoctors]] = await Promise.all([
+    db
+    .select({
+      total: sum(appointmentsTable.appointmentPriceInCents),
+    })
+    .from(appointmentsTable)
+    .where(
+      and(
+        eq(appointmentsTable.clinicId, session.user.clinic?.id),
+        gte(appointmentsTable.date, new Date(from)),
+        lte(appointmentsTable.date, new Date(to)),
+      ),
+    ),
+     db
+      .select({
+        total: count(),
+      })
+      .from(appointmentsTable)
+      .where(
+        and(
+          eq(appointmentsTable.clinicId, session.user.clinic?.id),
+          gte(appointmentsTable.date, new Date(from)),
+          lte(appointmentsTable.date, new Date(to)),
+        ),
+      ),
+      db
+    .select({
+      total: count(),
+    })
+    .from(patientsTable)
+    .where(eq(patientsTable.clinicId, session.user.clinic.id)),
+    db
+    .select({
+      total: count(),
+    })
+    .from(doctorsTable)
+    .where(eq(doctorsTable.clinicId, session.user.clinic.id)),
+  ]);
+
   return (
-     <PageContainer>
-        <PageHeader>
-          <PageHeaderContent>
-            <PageTitle>Pacientes</PageTitle>
-            <PageDescription>
-              Gerencie os pacientes da sua clínica
-            </PageDescription>
-          </PageHeaderContent>
-          <PageActions>
-            <DatePicker/>
-          </PageActions>
-        </PageHeader>
-        <PageContent>
-          <></>
-        </PageContent>
-      </PageContainer>
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Dashboard</PageTitle>
+          <PageDescription>
+            Tenha uma visão geral da sua clínica.
+          </PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <DatePicker />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <StatsCards
+          totalRevenue={totalRevenue.total ? Number(totalRevenue.total) : null}
+          totalAppointments={totalAppointments.total}
+          totalPatients={totalPatients.total}
+          totalDoctors={totalDoctors.total}
+        />
+      </PageContent>
+    </PageContainer>
   );
 };
 
